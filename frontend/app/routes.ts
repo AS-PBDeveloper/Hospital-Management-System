3a5644c934721d6ed02645da506624c0dba72522
Angkita Paul <paulankita614@gmail.com>
2026-09-10 14:24:06 +0530

    feat: remove unstable React Server Components documentation
    style: update app.css for improved theming and layout
    
    feat: enhance routing with login and protected routes
    
    chore: update dependencies in bun.lock and package.json
    
    feat: add login schema validation using zod
    
    feat: create CustomInput component for form handling
    
    feat: implement Login route with form and validation
    
    feat: add placeholder routes for protected components

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,22 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("login", "routes/Login.tsx"),
+
+  layout("routes/protected/layout.tsx", [
+    route("dashboard", "routes/protected/Dashboard.tsx"),
+    route("admins", "routes/protected/Admins.tsx"),
+    // route("doctors", "routes/protected/Doctors.tsx"),
+    // route("nurses", "routes/protected/Nurses.tsx"),
+    // route("patients", "routes/protected/Patients.tsx"),
+    // route("activities-log", "routes/protected/ActivitiesLog.tsx"),
+    // route("profile/:id", "routes/protected/Profile.tsx"),
+    // route("financial-history", "routes/protected/FinancialHistory.tsx"),
+  ]),
+] satisfies RouteConfig;
